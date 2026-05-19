bba7c664ffe5ce9dad356c02325cc7f9a6a0d431
Aayush Poudel <poudelaayush2006@gmail.com>
2026-05-19 14:32:08 +1000

    .

--- db_actions.py
+++ db_actions.py
@@ -1,30 +1,55 @@
 
 # get student by "username"
+def get_student_by_username():
+    return
 
 # get group by "group name"
+def get_group_by_groupname():
+    return
 
 # check if a certain user session exists (you are given just the session id)
+def check_valid_user_session():
+    return
 
 # check if a certain group session exists (you are given just the session id)
+def check_valid_group_session():
+    return
 
 # new session for student
+def new_student_session():
+    return
 
 # new session for group
+def new_group_session():
+    return
 
 # new student
+def new_student():
+    return
 
 # new group
+def new_group():
+    return
 
 # get student details
+def get_student_details():
+    return
 
 # update student details
+def update_student_details():
+    return
 
 # new attendance form
+def new_attendance_form():
+    return
 
 # user fills form
+def user_fills_form():
+    return
 
 # group check attedance for a particular form
-
+def group_check_attendance_form():
+    return
 
 
 
--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import Column, Integer, String, JSON, bool
 from database import Base
 
 class AttendanceForm(Base):
@@ -11,8 +11,34 @@ class AttendanceForm(Base):
 class Student(Base):
     __tablename__ = "students"
 
-    id = Column(Integer, primary_key=True, index=True)
-    event_id = Column(Integer)
-    data = Column(JSON)
+    first_name = Column(String)
+    last_name = Column(String)
+    age = Column(Integer)
+    zid = Column(String)
+    email = Column(String)
+    school_email = Column(String)
+    start_year = Column(Integer)
+    end_year = Column(Integer)
+    arc_clubs_joined = Column(JSON)
+    password = Column(String)
 
-class 
+class Group(Base):
+    __tablename__ = "groups"
+    name = Column(String)
+    members = Column(JSON)  # List of student IDs
+    
+
+class AttendanceForm(Base):
+    __tablename__ = "attendance_forms"
+    title = Column(String)
+    fields = Column(JSON)
+    responses = Column(JSON)
+
+class FormField(Base):
+    name = Column(String)
+    label = Column(String)
+    field_type = Column(String)  # e.g., "text", "number", "email", etc.
+    required = Column(bool)
+    prefill = Column(bool)  # Optional prefill value for the field
+ 
+    
